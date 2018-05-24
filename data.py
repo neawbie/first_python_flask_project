0398d56c05465427ed1d3b0c4ded68d9a7bcd853
Domagoj Bosančić <domagoj@MBP.local>
2018-05-24 18:16:29 +0200

    lol

--- data.py
+++ data.py
@@ -4,21 +4,21 @@ def Articles():
 			'id': 1,
 			'title': 'Artilce One',
 			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			'authore': "Domagoj",
+			'author': "Domagoj",
 			'create_date': '19-05-2018'
 		},
 		{
 			'id': 2,
 			'title': 'Artilce Two',
 			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			'authore': "Bob",
+			'author': "Bob",
 			'create_date': '19-05-2018'
 		},
 		{
 			'id': 3,
 			'title': 'Artilce Tree',
 			'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-			'authore': "Marko",
+			'author': "Marko",
 			'create_date': '19-05-2018'
 		}
 	]
